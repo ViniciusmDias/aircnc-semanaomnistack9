019f75076719429b04d81bb222b699515b70d475
Vinicius Monteiro Dias <vinicius-mdias@hotmail.com>
2019-10-05 14:44:42 -0300

    Cria o frontend do projeto junto com a página inicial

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -14,7 +15,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-mpxvf.mongodb.net/
 //req.query = Acessar query params (para filtros)
 //req.params = Acessar route params (para edição e deletes)
 //req.body = Acessar corpo da requisição (criação e edição de registros)
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
