be306cd92882171b2e1dd55a8e9b601f5aab77df
Vinicius Monteiro Dias <vinicius-mdias@hotmail.com>
2019-10-05 18:44:21 -0300

    Finaliza a página Dashboard

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
+
 const routes = require('./routes');
 
 const app = express();
@@ -17,6 +20,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-mpxvf.mongodb.net/
 //req.body = Acessar corpo da requisição (criação e edição de registros)
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
