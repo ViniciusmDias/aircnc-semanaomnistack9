a51b39155e6b70e62d878ca6fb33a4f6e0b75dd9
Vinicius Monteiro Dias <vinicius-mdias@hotmail.com>
2019-10-04 21:22:50 -0300

    Cria os schemas de booking e spots

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
+
 const sessionController = require('./controllers/SessionController')
+const spotController = require('./controllers/SpotController')
+const dashboardController = require('./controllers/DashboardController')
+const bookingController = require('./controllers/BookingController')
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', sessionController.store);
+routes.get('/spots', spotController.index);
+routes.post('/spots', upload.single('thumbnail'), spotController.store);
 
+routes.get('/dashboard', dashboardController.show);
 
+routes.post('/spots/:spot_id/bookings', bookingController.store);
 
 
 module.exports = routes;
