0f1d87ea4f7fcb35b03fa85e387a784faf1db13e
Vinicius Monteiro Dias <vinicius-mdias@hotmail.com>
2019-10-06 21:26:54 -0300

    Cria a parte mobile do projeto e finaliza a página de Login

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect }  from 'react';
+import { View, SafeAreaView, Text, StyleSheet , AsyncStorage } from 'react-native';
+
+import SpotList from '../components/SpotList'
+
+import logo from '../assets/logo.png'
+
+export default function List()  {
+    const  [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
+
+    return ( 
+    <SafeAreaView style={styles.container}>
+        <Image style={styles.logo} source={logo} /> 
+        <SpotList></SpotList>
+    </SafeAreaView>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    logo: {
+        height: 32,
+        resizeMode: "contain",
+        alignSelf: 'center',
+        marginTop: 10,
+    },
+});
